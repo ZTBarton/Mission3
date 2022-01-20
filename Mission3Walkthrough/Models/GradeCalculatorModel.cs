ea920f5d94e8001f13963bd2dcdb220baafa1ede
Zachary Barton <75874562+ZTBarton@users.noreply.github.com>
2022-01-19 22:28:36 -0700

    Initial check-in of module Mission3Walkthrough

---
+++ Mission3Walkthrough/Models/GradeCalculatorModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission3Walkthrough.Models
+{
+    public class GradeCalculatorModel
+    {
+        [Range(0, 100)]
+        public int assignmentGrade { get; set; }
+        //[Required]
+        [Range(0, 100)]
+        public int groupGrade { get; set; }
+        //[Required]
+        [Range(0, 100)]
+        public int quizGrade { get; set; }
+        //[Required]
+        [Range(0, 100)]
+        public int examGrade { get; set; }
+        //[Required]
+        [Range(0, 100)]
+        public int intexGrade { get; set; }
+
+        //public GradeCalculatorModel()
+        //{
+
+        //}
+    }
+}
